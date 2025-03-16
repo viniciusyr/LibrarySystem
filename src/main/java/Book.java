3815f744be9d10e4139fb644ffceb94383f3e3fb
Vinicius Rodrigues <vinicius.worms@hotmail.com>
2025-03-16 19:27:15 +0800

    Creating book class

---
+++ src/main/java/Book.java
@@ -0,0 +1,47 @@
+public class Book {
+
+    private int id;
+    private String title;
+    private int year;
+    private String author;
+
+    public Book(String title, int year, String author) {
+        this.title = title;
+        this.year = year;
+        this.author = author;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    
+}
