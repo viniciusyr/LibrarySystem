ae34f58f9aae7823e7a32d550141440fb9fee225
Vinicius Rodrigues <vinicius.worms@hotmail.com>
2025-03-18 21:01:29 +0800

    Created the get and close DB connection

--- src/main/java/DB.java
+++ src/main/java/DB.java
@@ -2,9 +2,16 @@ import exception.DBException;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.Properties;
 
 public class DB {
+
+    private static Connection conn = null;
+
     public static Properties loadProperties(){
         try(FileInputStream fs = new FileInputStream("src/db.properties")){
             Properties props = new Properties();
@@ -14,4 +21,28 @@ public class DB {
             throw new DBException(e.getMessage());
         }
     }
+
+    public static Connection getConnection(){
+        if(conn == null){
+            try{
+                Properties props = DB.loadProperties();
+                String url = props.getProperty("dburl");
+                conn = DriverManager.getConnection(url, props);
+            } catch (SQLException e) {
+                throw new DBException(e.getMessage());
+            }
+        }
+        return conn;
+    }
+
+    public static void closeConnection(Connection conn){
+        if(conn != null){
+            try{
+                conn.close();
+            } catch (SQLException e) {
+                throw new DBException(e.getMessage());
+            }
+        }
+    }
 }
+
